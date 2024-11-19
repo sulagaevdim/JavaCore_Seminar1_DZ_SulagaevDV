5fbbe8a040d97225aa430a87a0b268fca4be6f62
Дмитрий <sulagaevdim@mail.ru>
2024-11-19 17:06:51 +0400

    add dz

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.gb.sulagaev","l":"Decorator"},{"p":"ru.gb.sulagaev","l":"Main"},{"p":"ru.gb.sulagaev","l":"OtherClass"}];updateSearchResults();
